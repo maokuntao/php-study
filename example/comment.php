74314cc790c7e40a90b5fb77200a2c1f98178328
taomaokun <taomaokun@baidu.com>
2017-12-04 16:20:31 +0800

    add comment.php

---
+++ example/comment.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: taomaokun
+ * Date: 2017/12/4
+ * Time: 下午4:16
+ */
+echo "This is a test";// This is a one-line c++ style comment.
+/*
+ * This is a multi line comment yet another line of comment.
+ */
+echo "This ia yes another test";
+echo 'One Final Test';# This ia a one-line shell-style comment.
